dfcbbef68643133c1870c16a67cfc411ed0823a6
Ryba <julianryba@wp.pl>
2020-12-14 11:56:17 +0100

    Nowy plik
    Programowanie aplikacji intenetowych 15.12.2020

---
+++ aplikacjeinternetowe/2020-12-15/main.js
@@ -0,0 +1,16 @@
+var parametr
+str = new String
+
+function zad1(){
+    console.log('Udało się!')
+}
+function zad2(){
+    parametr = prompt('Podaj dowolny parametr:')
+    console.log(parametr)
+}
+function zad3(){
+    for(str=1; str<=5; str++){
+        console.log(str)
+    }
+    console.log("Koniec")
+}
